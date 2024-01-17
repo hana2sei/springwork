b4c074cd29f8c3992faf94de3e37df712a85ac4e
EZPAD\hana2 <hana2sei@gmail.com>
2024-01-17 22:07:15 +0900

    3.2 画面から別画面に値を渡す

--- SpringBootSample/src/main/java/com/example/demo/controller/HelloController.java
+++ SpringBootSample/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -9,5 +12,13 @@ public class HelloController {
 	@GetMapping("/hello")
 	public String getHello() {
 		return "cp1/hello";
+	}
+	
+	@PostMapping("/hello")
+	public String postRequest(@RequestParam("text1") String str, @RequestParam("weather") String str2, Model model) {
+		model.addAttribute("sample", str);
+		model.addAttribute("tenki", str2);
+		
+		return "cp1/response";
 	}
 }
